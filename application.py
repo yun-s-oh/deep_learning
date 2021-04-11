bfa6b9284321e6adddacb608bbeae7f71a6059db
Yunseok Oh <yun.oh@outlook.com>
2021-04-11 15:57:01 +1000

    Update face recognition

--- application.py
+++ application.py
@@ -4,9 +4,12 @@ from flask_session import Session
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.utils import secure_filename
 from tempfile import mkdtemp
+import face_recognition
+from PIL import Image, ImageDraw
+import numpy as np
+
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'
-}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 # Configure application
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -43,8 +46,20 @@ def index():
         if file and allowed_file(file.filename):
             filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
             file.save(filename)
+            image = face_recognition.load_image_file(filename)
+            face_locations = face_recognition.face_locations(image)
+            pil_image = Image.fromarray(image)
+            # Create a Pillow ImageDraw Draw instance to draw with
+            draw = ImageDraw.Draw(pil_image)
+            num_faces = len(face_locations)
+            app.logger.info('%s faces detected', len(face_locations))
+            for (top, right, bottom, left) in face_locations:
+                # Draw a box around the face using the Pillow module
+                draw.rectangle(((left, top), (right, bottom)), outline= 'red', width  = 2)
+
+            pil_image.save(filename)
             app.logger.info('file is saved as %s', filename)
-            return render_template("upload.html", user_image = filename)
+            return render_template("upload.html", user_image = filename, num_faces = num_faces)
 
 def errorhandler(e):
     """Handle error"""
